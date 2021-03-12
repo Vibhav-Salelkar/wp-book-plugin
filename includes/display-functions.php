42a27a8f610735475787def25b83e4fcfcd37bf0
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-03-12 09:49:36 +0530

    initial commit

---
+++ includes/display-functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: wp-book
+ * Description: Organizes book records
+ * Author: Vibhav Salelkar
+ * Version: 1.0 
+ * @return void
+ */
+
+ /**
+  *  Global Variables
+  */
+  $wb_my_plugin_name = "WP-BOOK";
+
+  /**
+   * Includes
+   */
