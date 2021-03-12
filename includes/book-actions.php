a4732b576c883ff8de009a26373bb72e0c9c11f3
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-03-12 10:38:21 +0530

    feat: book category taxonomy

--- includes/book-actions.php
+++ includes/book-actions.php
@@ -1,7 +1,7 @@
 <?php
 
 function wb_custom_post_type() {
-    register_post_type('wb_book',
+    register_post_type('book',
         array(
             'labels'      => array(
                 'name'          => __( 'Books', 'textdomain' ),
@@ -14,3 +14,31 @@ function wb_custom_post_type() {
     );
 }
 add_action('init', 'wb_custom_post_type');
+
+  
+function wb_register_taxonomy_book() {
+    $labels = array(
+        'name'              => _x( 'Book Category', 'taxonomy general name' ),
+        'singular_name'     => _x( 'Book', 'taxonomy singular name' ),
+        'search_items'      => __( 'Search Books' ),
+        'all_items'         => __( 'All Books' ),
+        'parent_item'       => __( 'Parent Book' ),
+        'parent_item_colon' => __( 'Parent Book:' ),
+        'edit_item'         => __( 'Edit Book' ),
+        'update_item'       => __( 'Update Book' ),
+        'add_new_item'      => __( 'Add New Book' ),
+        'new_item_name'     => __( 'New Book Name' ),
+        'menu_name'         => __( 'Book' ),
+    );
+    $args   = array(
+        'hierarchical'      => true, // make it hierarchical (like categories)
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_in_rest'      => true,    //required to see taxonomy in ui
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => [ 'slug' => 'book' ],
+    );
+    register_taxonomy( 'book', [ 'book' ], $args );
+}
+add_action( 'init', 'wb_register_taxonomy_book',0);
