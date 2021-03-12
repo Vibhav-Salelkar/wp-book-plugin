b4045dbf177e438a880d6b1e2da34d227a2a77c1
Vibhav Salelkar <salelkarvibhav@gmail.com>
2021-03-12 11:45:52 +0530

    feat: non heirarchical taxonomy tag

--- includes/book-actions.php
+++ includes/book-actions.php
@@ -42,3 +42,38 @@ function wb_register_taxonomy_book() {
     register_taxonomy( 'book', [ 'book' ], $args );
 }
 add_action( 'init', 'wb_register_taxonomy_book',0);
+
+ 
+function wb_nonhierarchical_taxonomy_book() {
+  
+  $labels = array(
+    'name' => _x( 'Book Tag', 'taxonomy general name' ),
+    'singular_name' => _x( 'Book Tag', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Book Tag' ),
+    'popular_items' => __( 'Popular Book Tags' ),
+    'all_items' => __( 'All Book Tags' ),
+    'parent_item' => null,
+    'parent_item_colon' => null,
+    'edit_item' => __( 'Edit Book Tag' ), 
+    'update_item' => __( 'Update Book Tag' ),
+    'add_new_item' => __( 'Add New Book Tag' ),
+    'new_item_name' => __( 'New Book Tag' ),
+    'separate_items_with_commas' => __( 'Separate book tags with commas' ),
+    'add_or_remove_items' => __( 'Add or remove book tags' ),
+    'choose_from_most_used' => __( 'Choose from the most used book tags' ),
+    'menu_name' => __( 'Book Tags' ),
+  ); 
+ 
+  register_taxonomy('book tags','book',array(
+    'hierarchical' => false,
+    'labels' => $labels,
+    'show_ui' => true,
+    'show_in_rest' => true,
+    'show_admin_column' => true,
+    'update_count_callback' => '_update_post_term_count',
+    'query_var' => true,
+    'rewrite' => array( 'slug' => 'book-tag' ),
+  ));
+}
+
+add_action( 'init', 'wb_nonhierarchical_taxonomy_book', 0 );
